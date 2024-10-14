f97edc0f46c1af5f5844b6a3b265ea01f3144f06
LordPachimari <opachimari@gmail.com>
2024-10-14 15:38:05 +0300

    what

--- apps/web/load-context.ts
+++ apps/web/load-context.ts
@@ -5,12 +5,7 @@ import type {
 	AuthSession,
 } from "@blazzing-app/validators";
 import type { Session, SessionData } from "@remix-run/cloudflare";
-// When using `wrangler.toml` to configure bindings,
-// `wrangler types` will generate types for those bindings
-// into the global `Env` interface.
-// Need this empty interface so that typechecking passes
-// even if no `wrangler.toml` exists.
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
+
 
 declare module "@remix-run/cloudflare" {
 	interface AppLoadContext {
