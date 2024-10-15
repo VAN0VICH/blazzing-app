f489545cf0f1440323a48429565ba16aeb5e9792
LordPachimari <opachimari@gmail.com>
2024-10-15 14:22:49 +0300

    fix

--- packages/functions/src/api/replicache.ts
+++ packages/functions/src/api/replicache.ts
@@ -3,173 +3,223 @@ import {
 	push,
 	ReplicacheContext,
 	staticPull,
-} from "@blazzing-app/replicache";
-import { AuthContext, Cloudflare, Database } from "@blazzing-app/shared";
-import {
+  } from "@blazzing-app/replicache";
+  import { AuthContext, Cloudflare, Database } from "@blazzing-app/shared";
+  import {
 	PullRequestSchema,
 	PushRequestSchema,
 	type SpaceRecord,
 	type WorkerBindings,
 	type WorkerEnv,
-} from "@blazzing-app/validators";
-import { zValidator } from "@hono/zod-validator";
-import { Effect, Layer } from "effect";
-import { Hono } from "hono";
-import { z } from "zod";
-import { getDB } from "../lib/db";
-import { getAuthUser } from "../lib/get-user";
-
-export namespace ReplicacheApi {
-	export const route = new Hono<{
-		Bindings: WorkerBindings & WorkerEnv;
+  } from "@blazzing-app/validators";
+  import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
+  import { Effect, Layer } from "effect";
+  import { getDB } from "../lib/db";
+  import { getAuthUser } from "../lib/get-user";
+  
+  export namespace ReplicacheApi {
+	export const route = new OpenAPIHono<{
+	  Bindings: WorkerBindings & WorkerEnv;
 	}>()
-		.post(
-			"/pull",
-			zValidator(
-				"query",
-				z.object({
-					spaceID: z.enum(["dashboard", "marketplace", "global"] as const),
-					subspaces: z.optional(z.array(z.string()).or(z.string())),
-				}),
-			),
-			zValidator("json", PullRequestSchema),
-			async (c) => {
-				// 1: PARSE INPUT
-				const authUser = await getAuthUser(c);
-				console.log("USER FROM PULL", authUser);
-				const db = getDB({ connectionString: c.env.DATABASE_URL });
-				const { spaceID, subspaces } = c.req.valid("query");
-				console.log("--------->SPACE ID<-------", spaceID);
-				const body = c.req.valid("json");
-				console.log("subspaceIDs", subspaces);
-
-				const CloudflareLive = Layer.succeed(
-					Cloudflare,
-					Cloudflare.of({
-						env: c.env,
-						request: c.req.raw,
-						bindings: {
-							KV: c.env.KV,
-						},
-					}),
-				);
-				const ReplicacheContextLive = Layer.succeed(
-					ReplicacheContext,
-					ReplicacheContext.of({
-						spaceID,
-						clientGroupID: body.clientGroupID,
-						subspaceIDs:
-							typeof subspaces === "string"
-								? [subspaces]
-								: (subspaces as SpaceRecord[typeof spaceID] | undefined),
-					}),
-				);
-
-				const AuthContextLive = Layer.succeed(
-					AuthContext,
-					AuthContext.of({
-						authUser,
-					}),
-				);
-
-				// 2: PULL
-				const pullEffect = pull({
-					body,
-					db,
-				}).pipe(
-					Effect.provide(AuthContextLive),
-					Effect.provide(CloudflareLive),
-					Effect.provide(ReplicacheContextLive),
-					Effect.orDie,
-				);
-
-				// 3: RUN PROMISE
-				const pullResponse = await Effect.runPromise(pullEffect);
-
-				return c.json(pullResponse, 200);
+	  .openapi(
+		createRoute({
+		  method: "post",
+		  path: "/pull",
+		  request: {
+			query: z.object({
+			  spaceID: z.enum(["dashboard", "marketplace", "global"] as const),
+			  subspaces: z.optional(z.array(z.string()).or(z.string())),
+			}),
+			body: {
+			  content: {
+				"application/json": {
+				  schema: PullRequestSchema,
+				},
+			  },
 			},
-		)
-		.post("/static-pull", zValidator("json", PullRequestSchema), async (c) => {
-			// 1: PARSE INPUT
-			const db = getDB({ connectionString: c.env.DATABASE_URL });
-			const body = c.req.valid("json");
-
-			// 2: PULL
-			const pullEffect = staticPull({ body }).pipe(
-				Effect.provideService(Database, { manager: db }),
-				Effect.provideService(
-					Cloudflare,
-					Cloudflare.of({
-						env: c.env,
-						request: c.req.raw,
-						bindings: {
-							KV: c.env.KV,
-						},
-					}),
-				),
-				Effect.orDie,
-			);
-
-			// 3: RUN PROMISE
-			const pullResponse = await Effect.runPromise(pullEffect);
-
-			return c.json(pullResponse, 200);
-		})
-		.post(
-			"/push",
-			zValidator(
-				"query",
-				z.object({
-					spaceID: z.enum(["dashboard", "marketplace", "global"] as const),
-				}),
-			),
-			zValidator("json", PushRequestSchema),
-			async (c) => {
-				const authUser = await getAuthUser(c);
-
-				// 1: PARSE INPUT
-				const db = getDB({ connectionString: c.env.DATABASE_URL });
-				const { spaceID } = c.req.valid("query");
-				const body = c.req.valid("json");
-
-				// 2: PULL
-				const pushEffect = push({
-					body,
-					db,
-					partyKitOrigin: c.env.PARTYKIT_ORIGIN,
-				}).pipe(
-					Effect.provideService(
-						AuthContext,
-						AuthContext.of({
-							authUser,
-						}),
-					),
-					Effect.provideService(
-						Cloudflare,
-						Cloudflare.of({
-							env: c.env,
-							request: c.req.raw,
-							bindings: {
-								KV: c.env.KV,
-							},
-						}),
-					),
-					Effect.provideService(
-						ReplicacheContext,
-						ReplicacheContext.of({
-							spaceID,
-							clientGroupID: body.clientGroupID,
-							subspaceIDs: undefined,
-						}),
-					),
-					Effect.scoped,
-					Effect.orDie,
-				);
-
-				// 3: RUN PROMISE
-				await Effect.runPromise(pushEffect);
-
-				return c.json({}, 200);
+		  },
+		  responses: {
+			200: {
+			  content: {
+				"application/json": {
+				  schema: z.any(), // Replace with actual response schema
+				},
+			  },
+			  description: "Successful pull operation",
 			},
-		);
-}
+		  },
+		}),
+		async (c) => {
+		  const authUser = await getAuthUser(c);
+		  console.log("USER FROM PULL", authUser);
+		  const db = getDB({ connectionString: c.env.DATABASE_URL });
+		  const { spaceID, subspaces } = c.req.valid("query");
+		  console.log("--------->SPACE ID<-------", spaceID);
+		  const body = c.req.valid("json");
+		  console.log("subspaceIDs", subspaces);
+  
+		  const CloudflareLive = Layer.succeed(
+			Cloudflare,
+			Cloudflare.of({
+			  env: c.env,
+			  request: c.req.raw,
+			  bindings: {
+				KV: c.env.KV,
+			  },
+			})
+		  );
+		  const ReplicacheContextLive = Layer.succeed(
+			ReplicacheContext,
+			ReplicacheContext.of({
+			  spaceID,
+			  clientGroupID: body.clientGroupID,
+			  subspaceIDs:
+				typeof subspaces === "string"
+				  ? [subspaces]
+				  : (subspaces as SpaceRecord[typeof spaceID] | undefined),
+			})
+		  );
+  
+		  const AuthContextLive = Layer.succeed(
+			AuthContext,
+			AuthContext.of({
+			  authUser,
+			})
+		  );
+  
+		  const pullEffect = pull({
+			body,
+			db,
+		  }).pipe(
+			Effect.provide(AuthContextLive),
+			Effect.provide(CloudflareLive),
+			Effect.provide(ReplicacheContextLive),
+			Effect.orDie
+		  );
+  
+		  const pullResponse = await Effect.runPromise(pullEffect);
+  
+		  return c.json(pullResponse, 200);
+		}
+	  )
+	  .openapi(
+		createRoute({
+		  method: "post",
+		  path: "/static-pull",
+		  request: {
+			body: {
+			  content: {
+				"application/json": {
+				  schema: PullRequestSchema,
+				},
+			  },
+			},
+		  },
+		  responses: {
+			200: {
+			  content: {
+				"application/json": {
+				  schema: z.any(), // Replace with actual response schema
+				},
+			  },
+			  description: "Successful static pull operation",
+			},
+		  },
+		}),
+		async (c) => {
+		  const db = getDB({ connectionString: c.env.DATABASE_URL });
+		  const body = c.req.valid("json");
+  
+		  const pullEffect = staticPull({ body }).pipe(
+			Effect.provideService(Database, { manager: db }),
+			Effect.provideService(
+			  Cloudflare,
+			  Cloudflare.of({
+				env: c.env,
+				request: c.req.raw,
+				bindings: {
+				  KV: c.env.KV,
+				},
+			  })
+			),
+			Effect.orDie
+		  );
+  
+		  const pullResponse = await Effect.runPromise(pullEffect);
+  
+		  return c.json(pullResponse, 200);
+		}
+	  )
+	  .openapi(
+		createRoute({
+		  method: "post",
+		  path: "/push",
+		  request: {
+			query: z.object({
+			  spaceID: z.enum(["dashboard", "marketplace", "global"] as const),
+			}),
+			body: {
+			  content: {
+				"application/json": {
+				  schema: PushRequestSchema,
+				},
+			  },
+			},
+		  },
+		  responses: {
+			200: {
+			  content: {
+				"application/json": {
+				  schema: z.object({}),
+				},
+			  },
+			  description: "Successful push operation",
+			},
+		  },
+		}),
+		async (c) => {
+		  const authUser = await getAuthUser(c);
+  
+		  const db = getDB({ connectionString: c.env.DATABASE_URL });
+		  const { spaceID } = c.req.valid("query");
+		  const body = c.req.valid("json");
+  
+		  const pushEffect = push({
+			body,
+			db,
+			partyKitOrigin: c.env.PARTYKIT_ORIGIN,
+		  }).pipe(
+			Effect.provideService(
+			  AuthContext,
+			  AuthContext.of({
+				authUser,
+			  })
+			),
+			Effect.provideService(
+			  Cloudflare,
+			  Cloudflare.of({
+				env: c.env,
+				request: c.req.raw,
+				bindings: {
+				  KV: c.env.KV,
+				},
+			  })
+			),
+			Effect.provideService(
+			  ReplicacheContext,
+			  ReplicacheContext.of({
+				spaceID,
+				clientGroupID: body.clientGroupID,
+				subspaceIDs: undefined,
+			  })
+			),
+			Effect.scoped,
+			Effect.orDie
+		  );
+  
+		  await Effect.runPromise(pushEffect);
+  
+		  return c.json({}, 200);
+		}
+	  );
+  }
