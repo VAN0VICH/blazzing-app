ec8b4942df7c9f7ae3871e46fc6fceca605b7aa2
LordPachimari <opachimari@gmail.com>
2024-12-03 01:33:23 +0300

    small ifx

--- packages/validators/src/shared/web.ts
+++ packages/validators/src/shared/web.ts
@@ -5,6 +5,8 @@ const WebEnvSchema = z.object({
 	REPLICACHE_KEY: z.string(),
 	ENVIRONMENT: z.enum(["production", "test", "staging", "development"]),
 	PARTYKIT_HOST: z.string().optional(),
+	LIVEKIT_SERVER_URL: z.string().optional(),
+	STRIPE_PUBLISHABLE_KEY: z.string().optional(),
 	BLAZZING_PUBLISHABLE_KEY: z.string(),
 	CLERK_PUBLISHABLE_KEY: z.string(),
 	CLERK_SECRET_KEY: z.string(),
@@ -15,6 +17,6 @@ const WebEnvSchema = z.object({
 });
 
 type WebEnv = z.infer<typeof WebEnvSchema>;
-type WebBindings = { SESSION_KV: KVNamespace; KV: KVNamespace };
+type WebBindings = { KV: KVNamespace };
 export { WebEnvSchema };
 export type { WebEnv, WebBindings };
